3e7b2cbef96a7cb43a28a71da9b143ca77030401
Gio2506 <91522962+Gio2506@users.noreply.github.com>
2021-12-08 15:42:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+char bid [10][20];
+for (int i=0;i<10;i++) {
+    char nome[20];
+    cout << "inserisci" << " " << "nome";
+    cin >> nome;
+    bid[i][20] = nome[20];
+}
+
+char mono[20];
+cin>>mono ;
+ArrayNomi (bid,mono);
+
+
+    return 0;
 }
