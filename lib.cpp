eb078268608396a5cd19049249d16b4e9ad2d105
Gio2506 <91522962+Gio2506@users.noreply.github.com>
2021-12-11 16:16:35 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,11 @@
 #include "lib.h"
 int ArrayNomi (char bid[10][20], char mono[20] ){
     for (int i=0;i<10;i++){
-        if (mono == bid[i]) {
-            return i;
-        }
+      bool si=true;
+      for (int j=0;j<20;j++){
+          if (bid [i][j]!=mono[j])si=false;
+      }
+      if(si)return i;
     }
+    return -1;
 }
-
-
